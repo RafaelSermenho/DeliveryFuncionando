d23ef8f2f3756cee5b71f64dcda6afbf85ad1ca2
Rafael Sermenho <rafael.sermenho@gmail.com>
2020-04-20 18:36:01 -0300

    Update layout and order

--- app/src/main/java/dev/rafaelsermenho/deliveryfuncionando/view/StoreAdapter.kt
+++ app/src/main/java/dev/rafaelsermenho/deliveryfuncionando/view/StoreAdapter.kt
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 import dev.rafaelsermenho.deliveryfuncionando.R
 import dev.rafaelsermenho.deliveryfuncionando.model.Store
 import kotlinx.android.synthetic.main.row_store.view.*
+import java.util.*
 
 class StoreAdapter(private val storeList: List<Store>) :
     RecyclerView.Adapter<StoreAdapter.ViewHolder>() {
@@ -22,18 +23,23 @@ class StoreAdapter(private val storeList: List<Store>) :
     }
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        private val name: TextView = view.txtNameValue
-        private val profile: TextView = view.txtProfileValue
-        private val product: TextView = view.txtProductsValue
-        private val workingHours: TextView = view.txtWorkingHoursValue
-        private val area: TextView = view.txtAreaValue
+        private val name: TextView = view.txtName
+        private val profile: TextView = view.txtProfile
+        private val product: TextView = view.txtProducts
+        private val workingHours: TextView = view.txtWorkingHours
+        private val contact: TextView = view.txtContact
+        private val city: TextView = view.txtCity
+        private val state: TextView = view.txtState
 
+        @UseExperimental(ExperimentalStdlibApi::class)
         fun bindView(store: Store) {
-            name.text = store.name
+            name.text = store.name.toLowerCase(Locale.getDefault()).capitalize(Locale.getDefault())
             profile.text = store.profile
             product.text = store.products
             workingHours.text = store.working_hours
-            area.text = store.area
+            contact.text = store.contact
+            city.text = store.city
+            state.text = store.state
         }
     }
 
